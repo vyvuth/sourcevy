1312f126e70e8ab30a9ea8be7202837d0f2d42c2
vyvuth <vyvuth191@gmail.com>
2025-03-08 22:50:09 +0700

    add first

---
+++ index.js
@@ -0,0 +1,16 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("source.json");
+const middleWare = jsonServer.defaults();
+const cors = require("cors");
+
+server.use(cors);
+server.use(middleWare);
+server.use(router);
+
+const PORT = 4000;
+const HOST = "localhost";
+
+server.listen(PORT, () => {
+  console.log(`Json Server is running on: http:// ${HOST}:${PORT}`);
+});
